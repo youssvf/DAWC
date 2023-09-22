0982af99b5f84d577f2f48b19b103f3a5a20bb81
Youssef <220238@fppiramide.com>
2023-09-22 12:17:20 +0200

    ..

---
+++ Tema1/Pruebas/script.js
@@ -0,0 +1,40 @@
+//1.
+
+    let a = 1357
+    let b = 135.7
+    let c = 135.7
+    let d = 135e7
+    let e = 0o1357
+    let f = 0x1A57
+
+    console.log(a)
+    console.log(typeof a)
+
+    console.log(b)
+    console.log(typeof b)
+    
+    console.log(c)
+    console.log(typeof c)
+    
+    console.log(d)
+    console.log(typeof d)
+
+    console.log(e)
+    console.log(typeof e)
+
+    console.log(f)
+    console.log(typeof f)
+
+//2.
+    
+//   let edad =  parseInt(prompt("Introduce tu edad"));
+//   console.log(edad, typeof edad)
+
+//3.
+
+    let nombre = prompt("Nombre:")
+    let apellidos = prompt("Apellidos:")
+    let edad1 = Number(prompt("Edad:")) 
+
+    console.log(nombre, apellidos, edad1)
+    
