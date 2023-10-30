1fa904c4de59490a3813cac497d52ef879fd074d
Youssef <220238@fppiramide.com>
2023-10-30 13:08:42 +0100

    30/10

--- DAWC/Tema 2/GestionEventos/script.js
+++ DAWC/Tema 2/GestionEventos/script.js
@@ -1,3 +1,5 @@
+
+
 class Persona{
     constructor(nombre, email){
         this.nombre =nombre;
@@ -9,7 +11,7 @@ class Alerta{
     constructor(fechahora,mensaje,timerId){
         this.fechahora = fechahora;
         this.mensaje = mensaje;
-        this.timerId = timerId;
+        this.timerId = 0;
     }
 }
 
@@ -22,21 +24,33 @@ class Evento{
         this.alertas = alertas;
     }
 
-    agregarInvitado(invitado){
-        this.invitados.push(invitado)
+    agregarInvitado(){
+        let nombre = prompt('Nombre del invitado');
+        let email = prompt('Email del invitado')
+        if(email.includes('@')){
+            this.invitados.push(new Persona(nombre,email))
+        } else {
+            alert('Email no válido')
+        }
     }
 
-    agregarAlerta(alerta){
-        this.alertas.push(alerta)
+    agregarAlerta(){
+        let fecha = prompt('Fecha alerta');
+        let mensaje = prompt('Mensaje alerta');
+        this.alertas.push(new Alerta(fecha,mensaje))
     }
 
     activarAlertas(){
         const now = new Date();
-        this.alertas = this.alertas.filter((alerta)=> {
-            if(alerta.fechahora > now){
-                return true;
+        this.alertas.forEach((alerta)=> {
+            const fechaalerta = new Date(alerta.fechahora);
+            if(fechaalerta > now){
+                const tiemporestante = fechaalerta - now;
+                alerta.timerId = setTimeout(()=>{
+                    alert(alerta.mensaje);
+                },tiemporestante);
             } else {
-                alert(alerta.mensaje);
+                
             }
         })
     }
@@ -54,17 +68,31 @@ class Evento{
         `)
         this.alertas.forEach((alerta)=>document.write(`<li>${alerta.fechahora}, ${alerta.mensaje}</li>`))
         document.write(`</ul>`)
-        //${this.invitados.map((invitado) => `<li>${invitado.nombre}, ${invitado.email}</li>`)}
     }
 }
 
+function crearEvento(){
+    let nombre = prompt('Nombre del evento');
+    let fecha = prompt('Fecha del evento');
+    let lugar = prompt('Lugar del evento');
 
+    agenda.push(new Evento(nombre,fecha,lugar));
+}
 
 function cargarAgenda(){
-    const AgendaLS = JSON.parse(localStorage.getItem('miAgenda')) ;
+    const AgendaLS = JSON.parse(localStorage.getItem('Agenda')) ;
     AgendaLS.forEach((evento)=>{
     agenda.push(new Evento(evento.nombre,evento.fechahora,evento.lugar,evento.invitados, evento.alertas));
-})
+    });
+    agenda.forEach((evento)=>{
+        evento.activarAlertas();
+    })
+
+    return agenda;
+}
+
+function guardarAgenda(){
+    localStorage.setItem("Agenda", JSON.stringify(agenda));
 }
 
 function agregarEvento(evento){
@@ -87,7 +115,48 @@ function borrarEvento(nombre){
 }
 
 function borrarEventosPasados(){
+    const now = new Date();
+    agenda.forEach((evento)=>{
+        let fechaevento = new Date(evento.fechahora);
+        if(now>fechaevento){
+            agenda.slice(evento)
+        }
+    })
+}
 
+function desactivarAlertas(nombre){
+    agenda.forEach((evento)=>{
+        if(evento.nombre == nombre){
+            evento.alertas.forEach((alerta)=>{
+                clearTimeout(alerta.timerId);   
+            })
+            alert(`Alertas de ${evento.nombre} desactivadas`)
+        }
+    })
+}
+
+function modificarEvento(nombre){
+    agenda.forEach((evento)=>{
+        if(evento.nombre == nombre){
+            evento.nombre = prompt('Nuevo nombre evento:');
+            evento.lugar = prompt('Nuevo lugar');
+            evento.fechahora = prompt('Nueva fecha');
+
+            let nombreinvitado = prompt('Nombre del invitado');
+            let email = prompt('Email del invitado');
+
+            const invitado = new Persona(nombreinvitado,email);
+
+            evento.invitados.push(invitado);
+
+            let fechaalerta = prompt('Fecha de la nueva alerta')
+            let mensaje = prompt('Mensaje de la alerta');
+
+            const alerta = new Alerta(fechaalerta,mensaje);
+
+            evento.alertas.push(alerta);
+        }
+    })
 }
 
 // let evento1 = new Evento('partido','2023-10-24T09:40','Camp nou');
@@ -103,9 +172,77 @@ function borrarEventosPasados(){
 // agregarEvento(evento2);
 
 
+
+
+
+
+// let evento1 = prompt('Nombre del evento')
+// agenda.forEach((evento)=>{
+//     if(evento.nombre === evento1){
+//         evento.mostrarEvento();
+//         evento.agregarAlerta();
+//         evento.activarAlertas();
+
+//         desactivarAlertas(evento.nombre)
+
+
+//     }
+// })
+
 const agenda = [];
 
-cargarAgenda();
+
+
+
+
+let menu = true;
+while(menu){
+    let respuesta = prompt('1- Crear evento 2-Añadir invitados  3-Añadir alertas  4-Mostrar eventos  5-Borrar evento  6-Borrar eventos pasado  7-Modificar evento  8-Desactivar alertas  9-Cargar Agenda  10-Guardar Agenda');
+    
+    switch(respuesta){
+    case '1' :
+        crearEvento();
+    break;
+
+    case '2':
+        let nombreevento = prompt('Nombre del evento')
+        agenda.forEach((evento)=>{
+            if(evento.nombre == nombreevento){
+                evento.agregarInvitado();
+            } else {
+                alert('Evento no encontrado');
+            }
+    });
+    break;
+
+    case '3':
+        let eventobuscado = prompt('Nombre del evento')
+        agenda.forEach((evento)=>{
+            if(evento.nombre == eventobuscado){
+                evento.agregarAlerta();
+            } else {
+                alert('Evento no encontrado');
+            }
+    });
+    break;
+
+    case '9':
+        agenda = cargarAgenda();
+        mostrarEventos();
+        
+    break;
+
+    case '10':
+        guardarAgenda();
+    break;
+
+    case '0' :
+        menu = false;
+    break;
+}
+}
+
+
 
 
 
