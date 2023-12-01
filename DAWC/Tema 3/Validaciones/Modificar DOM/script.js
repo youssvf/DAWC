47e6de003ab8be1688d03ec3823120b27c2724a3
Youssef <220238@fppiramide.com>
2023-12-01 13:49:27 +0100

    1/12

---
+++ DAWC/Tema 3/Validaciones/Modificar DOM/script.js
@@ -0,0 +1,15 @@
+function contarElementos(elemento){
+    alert(document.querySelectorAll(elemento).length);
+}
+
+function obtenerCaracteres(){
+    const parrafo = document.querySelectorAll("p")[0];
+    const texto = parrafo.textContent.slice(0,19);
+    alert(texto);
+}
+
+function
+
+// contarElementos("a");
+obtenerCaracteres();
+
