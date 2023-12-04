11cb680fc028b0cc206f7d3373d419f738f804ae
Youssef <220238@fppiramide.com>
2023-12-04 13:54:27 +0100

    4/12

---
+++ DAWC/Tema 3/Modificar DOM/script.js
@@ -0,0 +1,13 @@
+// alert(document.querySelectorAll('p').length);
+
+// alert(document.querySelectorAll("p")[0].textContent.slice(0, 19));
+
+// alert(document.querySelectorAll("*[href]").length);
+
+// alert(document.body.querySelectorAll("a[href]")[1]);
+
+// alert(document.querySelectorAll("a[href]")[document.querySelectorAll('a[href]').length-1]);
+
+// alert(document.querySelectorAll(`*[href$="https://www.cpifppiramide.com/"]`).length)
+
+alert(document.querySelectorAll('*[href]'))
