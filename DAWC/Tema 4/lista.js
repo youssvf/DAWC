bc6b5b29a741e9c7c38cbb0465e7eb719a3a6daf
Youssef <220238@fppiramide.com>
2023-12-15 14:20:18 +0100

    15/12

--- DAWC/Tema 4/lista.js
+++ DAWC/Tema 4/lista.js
@@ -1,4 +1,4 @@
-const URL_SERVER ="http://IP dE tu SERVIDOR:3000/";
+const URL_SERVER ="http://54.91.187.28:3000/";
 document.addEventListener("DOMContentLoaded",cargarListas,{once:true})
 
 function pintarListas(listas){
@@ -28,8 +28,28 @@ function pintarListas(listas){
     });
 }
 
-function cargarListas(){
+function ejemploGet() {
+    
+}
 
+function cargarListas(){
+    fetch(URL_BASE + "listas")
+        .then(response => {
+            if (response.ok) {//Si considero la respuesta correcta
+                return response.json();
+            } else {
+                //Aqui no me gusta la respuesta del servidor. 
+                //Opcion de lanzar un error, ojo, si no lo hago continua con el then siguiente
+            }
+        })
+        .then(data => {
+            //Aqui tengo lso datos en formato objet parseado del json recibido
+            console.log(typeof data, data);
+        })
+        .catch(error => {
+            //Gestion de errores
+            console.error(error);
+        })
 }
 
 function añadirListaNueva(){
