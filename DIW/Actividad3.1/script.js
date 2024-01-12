8e334d34a9f277535d0106d6af5515c696a4f0b3
Youssef <220238@fppiramide.com>
2024-01-13 00:11:03 +0100

    12/01

--- DIW/Actividad3.1/script.js
+++ DIW/Actividad3.1/script.js
@@ -1,36 +1,29 @@
-document.addEventListener('scroll', function() {
-    const scrollIndicator = document.getElementById('scrollIndicator');
-    const windowHeight = window.innerHeight;
-    const fullHeight = document.documentElement.scrollHeight;
-    const scrolled = window.scrollY;
+document.addEventListener("scroll", function () {
+  const scrollIndicator = document.getElementById("scrollIndicator");
+  const windowHeight = window.innerHeight;
+  const fullHeight = document.documentElement.scrollHeight;
+  const scrolled = window.scrollY;
 
-    const scrollPercent = (scrolled / (fullHeight - windowHeight)) * 100;
-    scrollIndicator.style.width = scrollPercent + '%';
+  const scrollPercent = (scrolled / (fullHeight - windowHeight)) * 100;
+  scrollIndicator.style.width = scrollPercent + "%";
 });
 
-
-
-
-  document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-      document.getElementById("loader").style.opacity = "0";
-      document.getElementById("content").style.opacity = "1";
-    }, 3000); // 3000 milliseconds (3 seconds)
-  });
-
-  document.addEventListener("DOMContentLoaded", function () {
-    // Obtener el elemento del loader
-    const loader = document.getElementById('loader');
-
-    // Establecer un temporizador para ocultar el loader después de 3 segundos
-    setTimeout(() => {
-        loader.style.opacity = '0'; // Hacer que el loader sea transparente
-        loader.style.display = 'none'; // Ocultar el loader
-    }, 3000); // 3000 milisegundos = 3 segundos
-
-    // Mostrar el loader inicialmente
-    loader.style.display = 'flex';
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("loader").style.opacity = "0";
+    document.getElementById("content").style.opacity = "1";
+  }, 3000); 
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  
+  const loader = document.getElementById("loader");
 
+  
+  setTimeout(() => {
+    loader.style.opacity = "0"; 
+    loader.style.display = "none"; 
+  }, 3000);
 
+  loader.style.display = "flex";
+});
