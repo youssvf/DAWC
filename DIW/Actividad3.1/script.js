1427226a7dc07f848df9bb029e2b839e7b8240fa
Youssef <220238@fppiramide.com>
2024-01-13 00:06:36 +0100

    12/01

--- DIW/Actividad3.1/script.js
+++ DIW/Actividad3.1/script.js
@@ -7,3 +7,30 @@ document.addEventListener('scroll', function() {
     const scrollPercent = (scrolled / (fullHeight - windowHeight)) * 100;
     scrollIndicator.style.width = scrollPercent + '%';
 });
+
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+      document.getElementById("loader").style.opacity = "0";
+      document.getElementById("content").style.opacity = "1";
+    }, 3000); // 3000 milliseconds (3 seconds)
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Obtener el elemento del loader
+    const loader = document.getElementById('loader');
+
+    // Establecer un temporizador para ocultar el loader después de 3 segundos
+    setTimeout(() => {
+        loader.style.opacity = '0'; // Hacer que el loader sea transparente
+        loader.style.display = 'none'; // Ocultar el loader
+    }, 3000); // 3000 milisegundos = 3 segundos
+
+    // Mostrar el loader inicialmente
+    loader.style.display = 'flex';
+});
+
+
+
